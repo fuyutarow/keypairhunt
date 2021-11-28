704e10a97479c5404510912fc7811975d5e86bb7
fuyutarow <fuyutarow@gmail.com>
2021-11-29 02:03:40 +0900

    Use ExpandedSecretKey

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+use ed25519_dalek::{ExpandedSecretKey, SecretKey};
 use serde::{Deserialize, Serialize};
 use solana_sdk::signer::keypair::Keypair;
 use std::{io::prelude::*, sync::Arc};
 use tokio::{io::AsyncWriteExt, sync::Mutex};
 
-#[derive(Debug, Default, Clone, Deserialize, Serialize)]
+use serde_big_array::big_array;
+big_array! { BigArray;}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
 struct Data {
     address: String,
-    secret_key: [u8; 32],
+    #[serde(with = "BigArray")]
+    secret_key: [u8; 64],
 }
 
 #[tokio::main]
@@ -21,10 +26,11 @@ async fn main() -> anyhow::Result<()> {
             loop {
                 let keypair = Keypair::new();
                 let address = keypair.to_base58_string();
+
                 if address.starts_with("Host") || address.to_lowercase().starts_with("xhost") {
                     let data = Data {
                         address,
-                        secret_key: keypair.secret().to_bytes(),
+                        secret_key: ExpandedSecretKey::from(keypair.secret()).to_bytes(),
                     };
                     let s = serde_json::to_string(&data).expect("serialize json");
 
@@ -37,7 +43,7 @@ async fn main() -> anyhow::Result<()> {
                     file.write(format!("{}\n", &s).as_bytes())
                         .await
                         .expect("write file");
-                    *count += 1;
+                    // *count += 1;
                     println!("{}", &s);
                 }
             }
